9e01bfc15054b4ac0fb3a38d3724f4d41c3aa461
gyugyu90 <kh2000park@gmail.com>
2019-07-03 11:19:35 +0900

    17장 변경 가능성을 최소화하라

---
+++ src/ch4/item17/Complex.java
@@ -0,0 +1,69 @@
+package ch4.item17;
+
+// 불변 복소수 클래스
+public final class Complex {
+
+    private final double re;
+    private final double im;
+
+    public static final Complex ZERO = new Complex(0, 0);
+    public static final Complex ONE = new Complex(1, 0);
+    public static final Complex I = new Complex(0, 1);
+
+
+    public Complex(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    public double realPart() {
+        return re;
+    }
+
+    public double imaginaryPart() {
+        return im;
+    }
+
+    public Complex plus(Complex c) {
+        return new Complex(re + c.re, im + c.im);
+    }
+
+    // 정적 팩토리(private 생성자와 함께 사용해야 한다)
+    public static Complex valueOf(double re, double im) {
+        return new Complex(re, im);
+    }
+
+    public Complex minus(Complex c) {
+        return new Complex(re - c.re, im - c.im);
+    }
+
+    public Complex times(Complex c) {
+        return new Complex(re * c.re, im * c.im);
+    }
+
+    public Complex dividedBy(Complex c) {
+        double tmp = c.re * c.re + c.im * c.im;
+        return new Complex((re * c.re + im * c.im) / tmp, (im * c.re - re * c.im) / tmp);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this)
+            return true;
+        if (!(obj instanceof Complex))
+            return false;
+        Complex c = (Complex) obj;
+
+        return Double.compare(c.re, re) == 0 && Double.compare(c.im, im) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * Double.hashCode(re) + Double.hashCode(im);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + re + " + " + im + ")";
+    }
+}
