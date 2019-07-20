5d52dde4d5bbee6df4dde218bc7d076ef18b8775
gyugyu90 <kh2000park@gmail.com>
2019-07-20 17:44:44 +0900

    18장 상속보다는 컴포지션을 사용하라

---
+++ src/ch4/ch18/InstrumentedHashSet.java
@@ -0,0 +1,42 @@
+package ch4.ch18;
+
+import com.sun.tools.javac.util.List;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class InstrumentedHashSet<E> extends HashSet<E> {
+
+    // 추가된 원소의 수
+    private int addCount = 0;
+
+    public InstrumentedHashSet() {
+
+    }
+
+    public InstrumentedHashSet(int initCap, float loadFactor) {
+        super(initCap, loadFactor);
+    }
+
+    @Override
+    public boolean add(E e) {
+        addCount++;
+        return super.add(e);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        addCount += c.size();
+        return super.addAll(c);
+    }
+
+    public int getAddCount() {
+        return addCount;
+    }
+
+    public static void main(String[] args) {
+        InstrumentedHashSet<String> s = new InstrumentedHashSet<>();
+        s.addAll(List.of("틱", "탁탁", "펑"));
+        System.out.println(s.getAddCount());
+    }
+}
